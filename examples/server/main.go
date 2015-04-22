f0fe901796a91ffc81ac5fff384bd7fc96897af4
iamqizhao <toqizhao@gmail.com>
2015-04-22 15:24:57 -0700

    Remove useless context setup

--- examples/server/main.go
+++ examples/server/main.go
@@ -166,10 +166,6 @@ func (s *_ABitOfEverythingServer) BulkEcho(stream examples.ABitOfEverythingServi
 }
 
 func run() error {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
 	l, err := net.Listen("tcp", ":9090")
 	if err != nil {
 		return err
