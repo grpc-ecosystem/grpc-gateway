9b4ca2e6ae5078e3639203fc6132e8b430d41c37
Yuki Yugui Sonoda <yugui@yugui.jp>
2015-08-17 21:07:36 +0900

    Extract example service impl to their own files

---
+++ examples/server/echo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	examples "github.com/gengo/grpc-gateway/examples/examplepb"
+	"github.com/golang/glog"
+	"golang.org/x/net/context"
+)
+
+// Implements of EchoServiceServer
+
+type echoServer struct{}
+
+func newEchoServer() examples.EchoServiceServer {
+	return new(echoServer)
+}
+
+func (s *echoServer) Echo(ctx context.Context, msg *examples.SimpleMessage) (*examples.SimpleMessage, error) {
+	glog.Info(msg)
+	return msg, nil
+}
+
+func (s *echoServer) EchoBody(ctx context.Context, msg *examples.SimpleMessage) (*examples.SimpleMessage, error) {
+	glog.Info(msg)
+	return msg, nil
+}
