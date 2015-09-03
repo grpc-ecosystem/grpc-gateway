6e590418e55e3004cc9907f6fef78e8efb3e77d0
Yuki Yugui Sonoda <yugui@yugui.jp>
2015-09-03 20:52:29 +0900

    Follow a change in protoc-gen-go
    Follows golang/protobuf@59b73b37c1e45995477aae817e4a653c89a858db,
    which introduces an incompatible change in generated codes

--- third_party/googleapis/google/api/annotations.pb.go
+++ third_party/googleapis/google/api/annotations.pb.go
@@ -10,14 +10,20 @@ It is generated from these files:
 	google/api/http.proto
 
 It has these top-level messages:
+	HttpRule
+	CustomHttpPattern
 */
 package google_api
 
 import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
+import math "math"
 import google_protobuf "github.com/golang/protobuf/protoc-gen-go/descriptor"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
+var _ = fmt.Errorf
+var _ = math.Inf
 
 var E_Http = &proto.ExtensionDesc{
 	ExtendedType:  (*google_protobuf.MethodOptions)(nil),
