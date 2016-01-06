9709b3cd5aa11def8c0c16fc593e3999fdd3ab5d
Travis Cline <travis.cline@gmail.com>
2016-01-05 21:06:46 -0800

    Improve PascalFromSnake behavior

--- utilities/name.go
+++ utilities/name.go
@@ -1,14 +1,52 @@
 package utilities
 
-import (
-	"strings"
-)
-
 // PascalFromSnake converts an identifier in snake_case into PascalCase.
-func PascalFromSnake(str string) string {
-	var components []string
-	for _, c := range strings.Split(str, "_") {
-		components = append(components, strings.Title(strings.ToLower(c)))
+func PascalFromSnake(s string) string {
+	// adopted from github.com/golang/protobuf/protoc-gen-go/generator/generator.go
+	if s == "" {
+		return ""
 	}
-	return strings.Join(components, "")
+	t := make([]byte, 0, 32)
+	i := 0
+	if s[0] == '_' {
+		// Need a capital letter; drop the '_'.
+		t = append(t, 'X')
+		i++
+	}
+	// Invariant: if the next letter is lower case, it must be converted
+	// to upper case.
+	// That is, we process a word at a time, where words are marked by _ or
+	// upper case letter. Digits are treated as words.
+	for ; i < len(s); i++ {
+		c := s[i]
+		if c == '_' && i+1 < len(s) && isASCIILower(s[i+1]) {
+			continue // Skip the underscore in s.
+		}
+		if isASCIIDigit(c) {
+			t = append(t, c)
+			continue
+		}
+		// Assume we have a letter now - if not, it's a bogus identifier.
+		// The next word is a sequence of characters that must start upper case.
+		if isASCIILower(c) {
+			c ^= ' ' // Make it a capital letter.
+		}
+		t = append(t, c) // Guaranteed not lower case.
+		// Accept lower case sequence that follows.
+		for i+1 < len(s) && isASCIILower(s[i+1]) {
+			i++
+			t = append(t, s[i])
+		}
+	}
+	return string(t)
+}
+
+// Is c an ASCII lower-case letter?
+func isASCIILower(c byte) bool {
+	return 'a' <= c && c <= 'z'
+}
+
+// Is c an ASCII digit?
+func isASCIIDigit(c byte) bool {
+	return '0' <= c && c <= '9'
 }
