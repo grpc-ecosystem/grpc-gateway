27796c1b09cfaa1e75d368fb87d070eacd2f7e3b
Yuki Yugui Sonoda <yugui@yugui.jp>
2016-05-09 11:29:48 +0900

    Fix style and lint errors

--- runtime/marshaler.go
+++ runtime/marshaler.go
@@ -11,23 +11,25 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
-const MIMEWILDCARD = "*"
+const mimeWildcard = "*"
 
-var inboundDefaultMarshaler = &JSONBuiltin{}
-var outboundDefaultMarshaler = &JSONBuiltin{}
-var contentTypeHeader = http.CanonicalHeaderKey("Content-Type")
+var (
+	inboundDefaultMarshaler  = new(JSONBuiltin)
+	outboundDefaultMarshaler = new(JSONBuiltin)
 
-// Get the inbound/outbound marshalers for this request. Checks the registry on the ServeMux for
-// the MIME type set by the Content-Type header.
+	contentTypeHeader = http.CanonicalHeaderKey("Content-Type")
+)
+
+// MarshalerForRequest returns the inbound/outbound marshalers for this request.
+// It checks the registry on the ServeMux for the MIME type set by the Content-Type header.
 // If it isn't set (or the request Content-Type is empty), checks for "*".
 // If that isn't set, uses the ServerMux's InboundMarshaler/OutboundMarshaler.
 // If there are multiple Content-Type headers set, choose the first one that it can
-// exactly match in the registry. Otherwise, follows the above logic for "*"/InboundMarshaler/OutboundMarshaler.
+// exactly match in the registry.
+// Otherwise, it follows the above logic for "*"/InboundMarshaler/OutboundMarshaler.
 func MarshalerForRequest(mux *ServeMux, r *http.Request) (inbound Marshaler, outbound Marshaler) {
-
 	inbound = nil
 	outbound = nil
-
 	headerVals := r.Header[contentTypeHeader]
 
 	for _, val := range headerVals {
@@ -41,7 +43,7 @@ func MarshalerForRequest(mux *ServeMux, r *http.Request) (inbound Marshaler, out
 				}
 			}
 		} else {
-			//Nil mimeMap, no need to bother checking for MIMEWILDCARD
+			//Nil mimeMap, no need to bother checking for mimeWildcard
 			if mux.InboundMarshaler == nil {
 				//Its nil, use our default
 				inbound = inboundDefaultMarshaler
@@ -64,7 +66,7 @@ func MarshalerForRequest(mux *ServeMux, r *http.Request) (inbound Marshaler, out
 	}
 
 	if mux.MIMERegistry != nil && mux.MIMERegistry.mimeMap != nil {
-		if m, ok := mux.MIMERegistry.mimeMap[MIMEWILDCARD]; ok {
+		if m, ok := mux.MIMERegistry.mimeMap[mimeWildcard]; ok {
 			if inbound == nil && m.inbound != nil {
 				inbound = m.inbound
 			}
@@ -74,7 +76,7 @@ func MarshalerForRequest(mux *ServeMux, r *http.Request) (inbound Marshaler, out
 		}
 	}
 
-	//Haven't gotten anywhere with any of the headers or MIMEWILDCARD
+	//Haven't gotten anywhere with any of the headers or mimeWildcard
 	//Try to use the mux, otherwise use our default
 	if inbound == nil {
 		if mux.InboundMarshaler == nil {
@@ -98,6 +100,7 @@ func MarshalerForRequest(mux *ServeMux, r *http.Request) (inbound Marshaler, out
 
 }
 
+// MarshalerMIMERegistry keeps a mapping from MIME types to mimeMarshalers.
 type MarshalerMIMERegistry struct {
 	mimeMap map[string]*mimeMarshaler
 }
@@ -107,10 +110,10 @@ type mimeMarshaler struct {
 	outbound Marshaler
 }
 
-// Add an inbound and outbund marshaler for a case-sensitive MIME type string ("*" to match any MIME type).
+// AddMarshaler adds an inbound and outbund marshaler for a case-sensitive MIME type string ("*" to match any MIME type).
 // Inbound is the marshaler that is used when marshaling inbound requests from the client.
 // Outbound is the marshaler that is used when marshaling outbound responses to the client.
-func (m *MarshalerMIMERegistry) AddMarshaler(mime string, inbound Marshaler, outbound Marshaler) {
+func (m *MarshalerMIMERegistry) AddMarshaler(mime string, inbound, outbound Marshaler) {
 
 	if len(mime) == 0 {
 		panic("Mime can't be an empty string")
@@ -123,7 +126,7 @@ func (m *MarshalerMIMERegistry) AddMarshaler(mime string, inbound Marshaler, out
 
 }
 
-// Add an inbound marshaler for a case-sensitive MIME type string ("*" to match any MIME type).
+// AddInboundMarshaler adds an inbound marshaler for a case-sensitive MIME type string ("*" to match any MIME type).
 // Inbound is the marshaler that is used when marshaling inbound requests from the client.
 func (m *MarshalerMIMERegistry) AddInboundMarshaler(mime string, inbound Marshaler) {
 
@@ -143,7 +146,7 @@ func (m *MarshalerMIMERegistry) AddInboundMarshaler(mime string, inbound Marshal
 
 }
 
-// Add an outbund marshaler for a case-sensitive MIME type string ("*" to match any MIME type).
+// AddOutboundMarshaler adds an outbund marshaler for a case-sensitive MIME type string ("*" to match any MIME type).
 // Outbound is the marshaler that is used when marshaling outbound responses to the client.
 func (m *MarshalerMIMERegistry) AddOutboundMarshaler(mime string, outbound Marshaler) {
 	mime = http.CanonicalHeaderKey(mime)
@@ -163,8 +166,9 @@ func (m *MarshalerMIMERegistry) AddOutboundMarshaler(mime string, outbound Marsh
 
 }
 
-// Create a new MIME marshaler registry. Allows for a mapping of case-sensitive
-// Content-Type MIME type string to runtime.Marshaler interfaces.
+// NewMarshalerMIMERegistry returns a new registry of marshalers.
+// It allows for a mapping of case-sensitive Content-Type MIME type string to runtime.Marshaler interfaces.
+//
 // For example, you could allow the client to specify the use of the runtime.JSONPb marshaler
 // with a "applicaton/jsonpb" Content-Type and the use of the runtime.JSONBuiltin marshaler
 // with a "application/json" Content-Type.
@@ -176,44 +180,65 @@ func NewMarshalerMIMERegistry() *MarshalerMIMERegistry {
 	}
 }
 
+// Marshaler defines a conversion between byte sequence and gRPC payloads / fields.
 type Marshaler interface {
+	// Marshal marshals "v" into byte sequence.
 	Marshal(v interface{}) ([]byte, error)
+	// Unmarshal unmarshals "data" into "v".
+	// "v" must be a pointer value.
 	Unmarshal(data []byte, v interface{}) error
+	// NewDecoder returns a Decoder which reads byte sequence from "r".
 	NewDecoder(r io.Reader) Decoder
+	// NewEncoder returns an Encoder which writes bytes sequence into "w".
 	NewEncoder(w io.Writer) Encoder
+	// ContentType returns the Content-Type which this marshaler is responsible for.
 	ContentType() string
 }
 
+// Decoder decodes a byte sequence
 type Decoder interface {
 	Decode(v interface{}) error
 }
 
+// Encoder encodes gRPC payloads / fields into byte sequence.
 type Encoder interface {
 	Encode(v interface{}) error
 }
 
+// JSONBuiltin is a Marshaler which marshals/unmarshals into/from JSON
+// with the standard "encoding/json" package of Golang.
+// Although it is generally faster for simple proto messages than JSONPb,
+// it does not support advanced features of protobuf, e.g. map, oneof, ....
 type JSONBuiltin struct{}
 
+// ContentType always Returns "application/json".
 func (*JSONBuiltin) ContentType() string {
 	return "application/json"
 }
 
+// Marshal marshals "v" into JSON
 func (j *JSONBuiltin) Marshal(v interface{}) ([]byte, error) {
 	return json.Marshal(v)
 }
 
+// Unmarshal unmarshals JSON data into "v".
 func (j *JSONBuiltin) Unmarshal(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
 
+// NewDecoder returns a Decoder which reads JSON stream from "r".
 func (j *JSONBuiltin) NewDecoder(r io.Reader) Decoder {
 	return json.NewDecoder(r)
 }
 
+// NewEncoder returns an Encoder which writes JSON stream into "w".
 func (j *JSONBuiltin) NewEncoder(w io.Writer) Encoder {
 	return json.NewEncoder(w)
 }
 
+// JSONPb is a Marshaler which marshals/unmarshals into/from JSON
+// with the "github.com/golang/protobuf/jsonpb".
+// It supports fully functionality of protobuf unlike JSONBuiltin.
 type JSONPb struct {
 	// Whether to render enum values as integers, as opposed to string values.
 	EnumsAsInts bool
@@ -231,10 +256,14 @@ type JSONPb struct {
 	OrigName bool
 }
 
+// ContentType always returns "application/json".
 func (*JSONPb) ContentType() string {
 	return "application/json"
 }
 
+// Marshal marshals "v" into JSON
+// Currently it can marshal only proto.Message.
+// TODO(yugui) Support fields of primitive types in a message.
 func (j *JSONPb) Marshal(v interface{}) ([]byte, error) {
 	m := &jsonpb.Marshaler{
 		EnumsAsInts:  j.EnumsAsInts,
@@ -242,31 +271,37 @@ func (j *JSONPb) Marshal(v interface{}) ([]byte, error) {
 		Indent:       j.Indent,
 		OrigName:     j.OrigName,
 	}
-	if p, ok := v.(proto.Message); ok {
-		var buf bytes.Buffer
-		if err := m.Marshal(&buf, p); err != nil {
-			return nil, err
-		}
-		return buf.Bytes(), nil
-	} else {
-		return nil, errors.New("Interface is not proto.Message")
+	p, ok := v.(proto.Message)
+	if !ok {
+		return nil, errors.New("interface is not proto.Message")
 	}
+
+	var buf bytes.Buffer
+	if err := m.Marshal(&buf, p); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 
 }
 
+// Unmarshal unmarshals JSON "data" into "v"
+// Currently it can marshal only proto.Message.
+// TODO(yugui) Support fields of primitive types in a message.
 func (j *JSONPb) Unmarshal(data []byte, v interface{}) error {
 	r := bytes.NewReader(data)
-	if p, ok := v.(proto.Message); ok {
-		return jsonpb.Unmarshal(r, p)
-	} else {
-		return errors.New("Interface is not proto.Message")
+	p, ok := v.(proto.Message)
+	if !ok {
+		return errors.New("interface is not proto.Message")
 	}
+	return jsonpb.Unmarshal(r, p)
 }
 
+// NewDecoder returns a Decoder which reads JSON stream from "r".
 func (j *JSONPb) NewDecoder(r io.Reader) Decoder {
-	return &JSONPbDecoder{decoder: json.NewDecoder(r)}
+	return &jsonPbDecoder{decoder: json.NewDecoder(r)}
 }
 
+// NewEncoder returns an Encoder which writes JSON stream into "w".
 func (j *JSONPb) NewEncoder(w io.Writer) Encoder {
 	m := &jsonpb.Marshaler{
 		EnumsAsInts:  j.EnumsAsInts,
@@ -275,33 +310,34 @@ func (j *JSONPb) NewEncoder(w io.Writer) Encoder {
 		OrigName:     j.OrigName,
 	}
 
-	return &JSONPbEncoder{
+	return &jsonPbEncoder{
 		marshal: m,
 		writer:  w,
 	}
 }
 
-type JSONPbDecoder struct {
+type jsonPbDecoder struct {
 	decoder *json.Decoder
 }
 
-func (j *JSONPbDecoder) Decode(v interface{}) error {
-	if p, ok := v.(proto.Message); ok {
-		return jsonpb.UnmarshalNext(j.decoder, p)
-	} else {
-		return errors.New("Interface is not proto.Message")
+func (j *jsonPbDecoder) Decode(v interface{}) error {
+	p, ok := v.(proto.Message)
+	if !ok {
+		return errors.New("interface is not proto.Message")
 	}
+
+	return jsonpb.UnmarshalNext(j.decoder, p)
 }
 
-type JSONPbEncoder struct {
+type jsonPbEncoder struct {
 	marshal *jsonpb.Marshaler
 	writer  io.Writer
 }
 
-func (j *JSONPbEncoder) Encode(v interface{}) error {
-	if p, ok := v.(proto.Message); ok {
-		return j.marshal.Marshal(j.writer, p)
-	} else {
-		return errors.New("Interface is not proto.Message")
+func (j *jsonPbEncoder) Encode(v interface{}) error {
+	p, ok := v.(proto.Message)
+	if !ok {
+		return errors.New("interface is not proto.Message")
 	}
+	return j.marshal.Marshal(j.writer, p)
 }
