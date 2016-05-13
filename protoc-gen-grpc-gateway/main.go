8f479afa55957012f8328025b98f0d3ac0e2b9a5
Yuki Yugui Sonoda <yugui@yugui.jp>
2016-05-13 10:49:48 +0900

    Add godoc comment

--- protoc-gen-grpc-gateway/main.go
+++ protoc-gen-grpc-gateway/main.go
@@ -1,3 +1,11 @@
+// Command protoc-gen-grpc-gateway is a plugin for Google protocol buffer
+// compiler to generate a reverse-proxy, which converts incoming RESTful
+// HTTP/1 requests gRPC invocation.
+// You rarely need to run this program directly. Instead, put this program
+// into your $PATH with a name "protoc-gen-grpc-gateway" and run
+//   protoc --grpc-gateway_out=output_directory path/to/input.proto
+//
+// See README.md for more details.
 package main
 
 import (
