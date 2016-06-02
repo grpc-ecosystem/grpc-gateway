f3df73216b2fafd31abdf0c859f16c326fa48a28
Yuki Yugui Sonoda <yugui@gengo.com>
2016-06-02 10:34:44 +0900

    Regenerate files with the latest protoc-gen-go

--- examples/examplepb/echo_service.pb.go
+++ examples/examplepb/echo_service.pb.go
@@ -44,7 +44,9 @@ var _ = math.Inf
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the proto package it is being compiled against.
-const _ = proto.ProtoPackageIsVersion1
+// A compilation error at this line likely means your copy of the
+// proto package needs to be updated.
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 // SimpleMessage represents a simple message sent to the Echo service.
 type SimpleMessage struct {
