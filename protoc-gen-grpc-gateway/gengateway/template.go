6193f51e51b0bfea774a33f535bc10e983832710
Travis Cline <travis.cline@gmail.com>
2017-01-07 10:28:37 -0800

    Prevent lack of http bindings from generating non-building output (#286)
    * Add example service with no bindings to demonstrate bug.
    
    * Skip services that have no methods with http bindings

--- protoc-gen-grpc-gateway/gengateway/template.go
+++ protoc-gen-grpc-gateway/gengateway/template.go
@@ -71,22 +71,26 @@ func applyTemplate(p param) (string, error) {
 	if err := headerTemplate.Execute(w, p); err != nil {
 		return "", err
 	}
-	var methodSeen bool
+	var targetServices []*descriptor.Service
 	for _, svc := range p.Services {
+		var methodWithBindingsSeen bool
 		for _, meth := range svc.Methods {
 			glog.V(2).Infof("Processing %s.%s", svc.GetName(), meth.GetName())
-			methodSeen = true
 			for _, b := range meth.Bindings {
+				methodWithBindingsSeen = true
 				if err := handlerTemplate.Execute(w, binding{Binding: b}); err != nil {
 					return "", err
 				}
 			}
 		}
+		if methodWithBindingsSeen {
+			targetServices = append(targetServices, svc)
+		}
 	}
-	if !methodSeen {
+	if len(targetServices) == 0 {
 		return "", errNoTargetService
 	}
-	if err := trailerTemplate.Execute(w, p.Services); err != nil {
+	if err := trailerTemplate.Execute(w, targetServices); err != nil {
 		return "", err
 	}
 	return w.String(), nil
