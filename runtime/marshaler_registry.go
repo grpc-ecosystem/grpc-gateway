2c613d9377bd885bb5709e42c178824ef7e71735
Cyril Tovena <cyril.tovena@gmail.com>
2018-02-08 17:23:46 -0500

    set the defaultMarshaler to not use original pb

--- runtime/marshaler_registry.go
+++ runtime/marshaler_registry.go
@@ -13,7 +13,7 @@ var (
 	acceptHeader      = http.CanonicalHeaderKey("Accept")
 	contentTypeHeader = http.CanonicalHeaderKey("Content-Type")
 
-	defaultMarshaler = &JSONPb{OrigName: true}
+	defaultMarshaler = &JSONPb{OrigName: false}
 )
 
 // MarshalerForRequest returns the inbound/outbound marshalers for this request.
