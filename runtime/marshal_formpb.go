2b49c7898349b57bbc8435d52d2c772097ede319
JINLEI1 <JINLEI1@kingsoft.com>
2018-02-13 15:23:20 +0800

    add form <-> json marshal

---
+++ runtime/marshal_formpb.go
@@ -0,0 +1,119 @@
+package runtime
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"reflect"
+
+	"github.com/gogo/protobuf/jsonpb"
+	"github.com/gogo/protobuf/proto"
+	"github.com/grpc-ecosystem/grpc-gateway/utilities"
+)
+
+// This Marshal is to parse application/x-www-form-urlencoded and return json
+// You should add `runtime.WithMarshalerOption("application/x-www-form-urlencoded", &runtime.FORMPb{}),`
+// before MIMEWildcard
+type FORMPb jsonpb.Marshaler
+
+func (j *FORMPb) NewDecoder(r io.Reader) Decoder {
+	return DecoderFunc(func(v interface{}) error { return decodeFORMPb(r, v) })
+}
+
+func decodeFORMPb(d io.Reader, v interface{}) error {
+	msg, ok := v.(proto.Message)
+
+	if !ok {
+		return fmt.Errorf("not proto message")
+	}
+
+	formData, err := ioutil.ReadAll(d)
+
+	if err != nil {
+		return err
+	}
+
+	values, err := url.ParseQuery(string(formData))
+
+	if err != nil {
+		return err
+	}
+
+	filter := &utilities.DoubleArray{}
+
+	err = PopulateQueryParameters(msg, values, filter)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (j *FORMPb) Unmarshal(data []byte, v interface{}) error {
+	return nil
+}
+
+func (*FORMPb) ContentType() string {
+	return "application/json"
+}
+
+func (j *FORMPb) Marshal(v interface{}) ([]byte, error) {
+	if _, ok := v.(proto.Message); !ok {
+		return j.marshalNonProtoField(v)
+	}
+
+	var buf bytes.Buffer
+	if err := j.marshalTo(&buf, v); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (j *FORMPb) marshalTo(w io.Writer, v interface{}) error {
+	p, ok := v.(proto.Message)
+	if !ok {
+		buf, err := j.marshalNonProtoField(v)
+		if err != nil {
+			return err
+		}
+		_, err = w.Write(buf)
+		return err
+	}
+	return (*jsonpb.Marshaler)(j).Marshal(w, p)
+}
+
+func (j *FORMPb) marshalNonProtoField(v interface{}) ([]byte, error) {
+	rv := reflect.ValueOf(v)
+	for rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			return []byte("null"), nil
+		}
+		rv = rv.Elem()
+	}
+
+	if rv.Kind() == reflect.Map {
+		m := make(map[string]*json.RawMessage)
+		for _, k := range rv.MapKeys() {
+			buf, err := j.Marshal(rv.MapIndex(k).Interface())
+			if err != nil {
+				return nil, err
+			}
+			m[fmt.Sprintf("%v", k.Interface())] = (*json.RawMessage)(&buf)
+		}
+		if j.Indent != "" {
+			return json.MarshalIndent(m, "", j.Indent)
+		}
+		return json.Marshal(m)
+	}
+	if enum, ok := rv.Interface().(protoEnum); ok && !j.EnumsAsInts {
+		return json.Marshal(enum.String())
+	}
+	return json.Marshal(rv.Interface())
+}
+func (j *FORMPb) NewEncoder(w io.Writer) Encoder {
+	return EncoderFunc(func(v interface{}) error { return j.marshalTo(w, v) })
+}
