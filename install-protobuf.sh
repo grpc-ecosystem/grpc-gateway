715d0bf184df7c59f9a3cef481e1b28c5f4a3faf
Heng Zhang <zhangheng1536@gmail.com>
2018-02-26 17:51:54 +0800

    Update install-protobuf.sh
    change protoc folder name

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-java-3.5.1.tar.gz
   tar -xzvf protobuf-java-3.5.1.tar.gz
-  cd protobuf-java-3.5.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  cd protobuf-3.5.1 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached directory."
 fi
