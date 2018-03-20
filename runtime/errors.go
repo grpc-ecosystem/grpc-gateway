bd2bfeb062ba9e1cc687abebd1e214a6d6036f35
Stan Nelson <eleniums@yahoo.com>
2018-03-20 10:08:42 -0700

    Changed to use more appropriate http status code for ResourceExhausted.

--- runtime/errors.go
+++ runtime/errors.go
@@ -35,7 +35,7 @@ func HTTPStatusFromCode(code codes.Code) int {
 	case codes.Unauthenticated:
 		return http.StatusUnauthorized
 	case codes.ResourceExhausted:
-		return http.StatusServiceUnavailable
+		return http.StatusRequestEntityTooLarge
 	case codes.FailedPrecondition:
 		return http.StatusPreconditionFailed
 	case codes.Aborted:
