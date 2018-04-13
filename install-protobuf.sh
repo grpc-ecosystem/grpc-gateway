4743ea0f03cde9b880483d65c8422bcaaa562353
ashu <ithinker1991@gmail.com>
2018-04-13 14:12:00 +0800

    fix install-protobuf.sh

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -e
 # check to see if protobuf folder is empty
-rm -rf $HOME/protobuf/lib
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.tar.gz
-  tar -xzvf protobuf-all-3.5.1.tar.gz
+  wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-java-3.5.1.tar.gz
+  tar -xzvf protobuf-java-3.5.1.tar.gz
   cd protobuf-3.5.1 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached directory."
