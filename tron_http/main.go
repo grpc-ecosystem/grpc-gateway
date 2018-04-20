f5a95f70961c4d09328319821c908b4059bc5ade
xiechang33 <354035097@qq.com>
2018-04-20 17:13:07 +0800

    fix 'Access-Control-Allow-Origin'.

--- tron_http/main.go
+++ tron_http/main.go
@@ -24,9 +24,9 @@ var (
 )
 
 func allowCORS(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HanedlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if origin := r.Header.Get("Origin"); origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
 				preflightHandler(w, r)
 				return
