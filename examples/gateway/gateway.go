cc855841fd088db6638be1326d8dc2616921fc63
Yuki Yugui Sonoda <yugui@yugui.jp>
2018-04-24 16:24:10 +0900

    Simplify the abstraction of network types

--- examples/gateway/gateway.go
+++ examples/gateway/gateway.go
@@ -2,70 +2,67 @@ package gateway
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"net/http"
 	"time"
 
+	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/examples/examplepb"
 	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 )
 
-type optSet struct {
-	mux  []gwruntime.ServeMuxOption
-	dial []grpc.DialOption
-
-	echoEndpoint, abeEndpoint, flowEndpoint string
-}
-
-// newGateway returns a new gateway server which translates HTTP into gRPC.
-func newGateway(ctx context.Context, opts optSet) (http.Handler, error) {
-	mux := gwruntime.NewServeMux(opts.mux...)
-
-	err := examplepb.RegisterEchoServiceHandlerFromEndpoint(ctx, mux, opts.echoEndpoint, opts.dial)
+// NewGateway returns a new gateway server which translates HTTP into gRPC.
+func NewGateway(ctx context.Context, network, addr string, opts []gwruntime.ServeMuxOption) (http.Handler, error) {
+	conn, err := dial(ctx, network, addr)
 	if err != nil {
 		return nil, err
 	}
-	err = examplepb.RegisterStreamServiceHandlerFromEndpoint(ctx, mux, opts.abeEndpoint, opts.dial)
-	if err != nil {
-		return nil, err
-	}
-	err = examplepb.RegisterABitOfEverythingServiceHandlerFromEndpoint(ctx, mux, opts.abeEndpoint, opts.dial)
-	if err != nil {
-		return nil, err
-	}
-	err = examplepb.RegisterFlowCombinationHandlerFromEndpoint(ctx, mux, opts.flowEndpoint, opts.dial)
-	if err != nil {
-		return nil, err
+	go func() {
+		<-ctx.Done()
+		if err := conn.Close(); err != nil {
+			glog.Errorf("Failed to close a client connection to the gRPC server: %v", err)
+		}
+	}()
+
+	mux := gwruntime.NewServeMux(opts...)
+
+	for _, f := range []func(context.Context, *gwruntime.ServeMux, *grpc.ClientConn) error{
+		examplepb.RegisterEchoServiceHandler,
+		examplepb.RegisterStreamServiceHandler,
+		examplepb.RegisterABitOfEverythingServiceHandler,
+		examplepb.RegisterFlowCombinationHandler,
+	} {
+		if err := f(ctx, mux, conn); err != nil {
+			return nil, err
+		}
 	}
 	return mux, nil
 }
 
-// NewTCPGateway returns a new gateway server which connect to the gRPC service with TCP.
-// "addr" must be a valid TCP address with a port number.
-func NewTCPGateway(ctx context.Context, addr string, opts ...gwruntime.ServeMuxOption) (http.Handler, error) {
-	return newGateway(ctx, optSet{
-		mux:          opts,
-		dial:         []grpc.DialOption{grpc.WithInsecure()},
-		echoEndpoint: addr,
-		abeEndpoint:  addr,
-		flowEndpoint: addr,
-	})
+func dial(ctx context.Context, network, addr string) (*grpc.ClientConn, error) {
+	switch network {
+	case "tcp":
+		return dialTCP(ctx, addr)
+	case "unix":
+		return dialUnix(ctx, addr)
+	default:
+		return nil, fmt.Errorf("unsupported network type %q", network)
+	}
 }
 
-// NewUnixGatway returns a new gateway server which connect to the gRPC service with a unix domain socket.
+// dialTCP creates a client connection via TCP.
+// "addr" must be a valid TCP address with a port number.
+func dialTCP(ctx context.Context, addr string) (*grpc.ClientConn, error) {
+	return grpc.DialContext(ctx, addr, grpc.WithInsecure())
+}
+
+// dialUnix creates a client connection via a unix domain socket.
 // "addr" must be a valid path to the socket.
-func NewUnixGateway(ctx context.Context, addr string, opts ...gwruntime.ServeMuxOption) (http.Handler, error) {
-	return newGateway(ctx, optSet{
-		mux: opts,
-		dial: []grpc.DialOption{
-			grpc.WithInsecure(),
-			grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
-				return net.DialTimeout("unix", addr, timeout)
-			}),
-		},
-		echoEndpoint: addr,
-		abeEndpoint:  addr,
-		flowEndpoint: addr,
-	})
+func dialUnix(ctx context.Context, addr string) (*grpc.ClientConn, error) {
+	d := func(addr string, timeout time.Duration) (net.Conn, error) {
+		return net.DialTimeout("unix", addr, timeout)
+	}
+	return grpc.DialContext(ctx, addr, grpc.WithInsecure(), grpc.WithDialer(d))
 }
