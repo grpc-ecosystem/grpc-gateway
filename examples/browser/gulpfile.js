66c14c7ebd82ec7916bef64cdbd79fa2589b1903
Yuki Yugui Sonoda <yugui@yugui.jp>
2018-04-24 18:18:10 +0900

    Move example servers under the same directory
    to keep them consistent and keep their subdirectories shallower.

--- examples/browser/gulpfile.js
+++ examples/browser/gulpfile.js
@@ -16,11 +16,11 @@ gulp.task('bower', function(){
 });
 
 gulp.task('server', shell.task([
-  'go build -o bin/example-server github.com/grpc-ecosystem/grpc-gateway/examples/server/cmd/example-server',
+  'go build -o bin/example-server github.com/grpc-ecosystem/grpc-gateway/examples/cmd/example-grpc-server',
 ]));
 
 gulp.task('gateway', shell.task([
-  'go build -o bin/example-gw github.com/grpc-ecosystem/grpc-gateway/examples',
+  'go build -o bin/example-gw github.com/grpc-ecosystem/grpc-gateway/examples/cmd/example-gateway-server',
 ]));
 
 gulp.task('serve-server', ['server'], function(){
