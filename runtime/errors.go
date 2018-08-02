1283180b18aa237bc79e80ddca8408130adae3d0
Fred Shih <ffred@google.com>
2018-08-02 16:42:33 -0700

    Add link to status proto in comment

--- runtime/errors.go
+++ runtime/errors.go
@@ -66,8 +66,9 @@ var (
 
 type errorBody struct {
 	Error   string     `protobuf:"bytes,1,name=error" json:"error"`
-	// This is to make the error more compatible with users that expect errors to be Status objects. It should be
-	// the exact same message as the Error field.
+	// This is to make the error more compatible with users that expect errors to be Status objects:
+	// https://github.com/grpc/grpc/blob/master/src/proto/grpc/status/status.proto
+	// It should be the exact same message as the Error field.
 	Message string     `protobuf:"bytes,1,name=message" json:"message"`
 	Code    int32      `protobuf:"varint,2,name=code" json:"code"`
 	Details []*any.Any `protobuf:"bytes,3,rep,name=details" json:"details,omitempty"`
