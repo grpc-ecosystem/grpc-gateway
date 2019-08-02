6d0c6dc7786c543bf01e5ea6dfb54464aba6a37c
Hobo86 <hobo@hbchen.com>
2019-08-03 07:31:49 +0800

    streaming return Unimplemented error

--- examples/proto/examplepb/unannotated_echo_service.pb.gw.go
+++ examples/proto/examplepb/unannotated_echo_service.pb.gw.go
@@ -258,30 +258,7 @@ func local_request_UnannotatedEchoService_EchoDelete_0(ctx context.Context, mars
 // RegisterUnannotatedEchoServiceHandlerServer registers the http handlers for service UnannotatedEchoService to "mux".
 // UnaryRPC     :call UnannotatedEchoServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
-// If the gateway proto have stream must add DialOption grpc.WithContextDialer. e.g.
-//
-//      bcLis := bufconn.Listen(1024 * 1024)
-// 		go s.Serve(bcLis)
-//
-// 		ctx := context.Background()
-// 		ctx, cancel := context.WithCancel(ctx)
-// 		defer cancel()
-//
-// 		mux := runtime.NewServeMux()
-// 		err := pb.RegisterUnannotatedEchoServiceHandlerServer(
-// 			ctx,
-// 			mux,
-// 			&srv,
-// 			[]grpc.DialOption{
-// 				grpc.WithContextDialer(func(ctx context.Context, s string) (net.Conn, error) {
-// 					return bcLis.Dial()
-// 				}),
-// 			},
-// 		)
-//
-// RegisterUnannotatedEchoServiceHandlerServer does not support stream rpc call directly, and grpc-go have an issue "Feature Request:
-// Add support for In-Process transport #906". So it is currently EXPERIMENTAL and subject to change.
-func RegisterUnannotatedEchoServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server UnannotatedEchoServiceServer, opts []grpc.DialOption) (err error) {
+func RegisterUnannotatedEchoServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server UnannotatedEchoServiceServer, opts []grpc.DialOption) error {
 
 	mux.Handle("POST", pattern_UnannotatedEchoService_Echo_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
--- examples/proto/examplepb/wrappers.pb.gw.go
+++ examples/proto/examplepb/wrappers.pb.gw.go
@@ -407,30 +407,7 @@ func local_request_WrappersService_CreateEmpty_0(ctx context.Context, marshaler 
 // RegisterWrappersServiceHandlerServer registers the http handlers for service WrappersService to "mux".
 // UnaryRPC     :call WrappersServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
-// If the gateway proto have stream must add DialOption grpc.WithContextDialer. e.g.
-//
-//      bcLis := bufconn.Listen(1024 * 1024)
-// 		go s.Serve(bcLis)
-//
-// 		ctx := context.Background()
-// 		ctx, cancel := context.WithCancel(ctx)
-// 		defer cancel()
-//
-// 		mux := runtime.NewServeMux()
-// 		err := pb.RegisterWrappersServiceHandlerServer(
-// 			ctx,
-// 			mux,
-// 			&srv,
-// 			[]grpc.DialOption{
-// 				grpc.WithContextDialer(func(ctx context.Context, s string) (net.Conn, error) {
-// 					return bcLis.Dial()
-// 				}),
-// 			},
-// 		)
-//
-// RegisterWrappersServiceHandlerServer does not support stream rpc call directly, and grpc-go have an issue "Feature Request:
-// Add support for In-Process transport #906". So it is currently EXPERIMENTAL and subject to change.
-func RegisterWrappersServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server WrappersServiceServer, opts []grpc.DialOption) (err error) {
+func RegisterWrappersServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server WrappersServiceServer, opts []grpc.DialOption) error {
 
 	mux.Handle("POST", pattern_WrappersService_Create_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
