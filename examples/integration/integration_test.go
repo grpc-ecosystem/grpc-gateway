d45ca0dce112c9f62c5a40a8e31da25dd28caa44
Will Plano <william.plano@oxbotica.com>
2019-09-27 14:27:56 +0100

    Move benchmark to fieldmask_test and just measure the FieldMaskFromRequestBody method

--- examples/integration/integration_test.go
+++ examples/integration/integration_test.go
@@ -670,10 +670,6 @@ func testABELookup(t *testing.T, port int) {
 // Then, issue a PATCH request updating only the string_value
 // Then, GET the resource and verify that string_value is changed, but int32_value isn't
 func TestABEPatch(t *testing.T) {
-	testABEPatch(t)
-}
-
-func testABEPatch(t testing.TB) {
 	port := 8080
 
 	// create a record with a known string_value and int32_value
@@ -739,7 +735,7 @@ func TestABEPatchBody(t *testing.T) {
 			},
 		},
 		{
-			// N.B. This case passes the empty field mask to the UpdateV2 method so falls back to PUT semantics as per the implementation.
+			// N.B. This case passes the empty field mask to the UpdateV2 method so falls back to PUT semantics as per the implementeation
 			name: "with empty fieldmask",
 			originalValue: gw.ABitOfEverything{
 				Int32Value:   42,
@@ -756,7 +752,7 @@ func TestABEPatchBody(t *testing.T) {
 			},
 		},
 		{
-			// N.B. This case passes the nil field mask to the UpdateV2 method so falls back to PUT semantics as per the implementation.
+			// N.B. This case passes the nil field mask to the UpdateV2 method so falls back to PUT semantics as per the implementeation
 			name: "with nil fieldmask",
 			originalValue: gw.ABitOfEverything{
 				Int32Value:   42,
@@ -809,7 +805,7 @@ func TestABEPatchBody(t *testing.T) {
 
 // mustMarshal marshals the given object into a json string, calling t.Fatal if an error occurs. Useful in testing to
 // inline marshalling whenever you don't expect the marshalling to return an error
-func mustMarshal(t testing.TB, i interface{}) string {
+func mustMarshal(t *testing.T, i interface{}) string {
 	b, err := json.Marshal(i)
 	if err != nil {
 		t.Fatalf("failed to marshal %#v: %v", i, err)
@@ -819,7 +815,7 @@ func mustMarshal(t testing.TB, i interface{}) string {
 }
 
 // postABE conveniently creates a new ABE record for ease in testing
-func postABE(t testing.TB, port int, abe gw.ABitOfEverything) (uuid string) {
+func postABE(t *testing.T, port int, abe gw.ABitOfEverything) (uuid string) {
 	apiURL := fmt.Sprintf("http://localhost:%d/v1/example/a_bit_of_everything", port)
 	postResp, err := http.Post(apiURL, "application/json", strings.NewReader(mustMarshal(t, abe)))
 	if err != nil {
@@ -843,7 +839,7 @@ func postABE(t testing.TB, port int, abe gw.ABitOfEverything) (uuid string) {
 }
 
 // getABE conveniently fetches an ABE record for ease in testing
-func getABE(t testing.TB, port int, uuid string) gw.ABitOfEverything {
+func getABE(t *testing.T, port int, uuid string) gw.ABitOfEverything {
 	gURL := fmt.Sprintf("http://localhost:%d/v1/example/a_bit_of_everything/%s", port, uuid)
 	getResp, err := http.Get(gURL)
 	if err != nil {
@@ -1703,13 +1699,5 @@ func testNonStandardNames(t *testing.T, port int, method string, jsonBody string
 
 	if got, want := string(body), jsonBody; got != want {
 		t.Errorf("got %q; want %q", got, want)
-	}
-}
-
-// This benchmark requires the number of iterations be fairly low i.e. `-benchtime 100x`
-// otherwise you'll start to see errors like: `dial tcp: lookup localhost: no such host`
-func BenchmarkPatchABE(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		testABEPatch(b)
 	}
 }
--- runtime/fieldmask_test.go
+++ runtime/fieldmask_test.go
@@ -66,3 +66,67 @@ func TestFieldMaskFromRequestBody(t *testing.T) {
 		})
 	}
 }
+
+// avoid compiler optimising benchmark away
+var result *field_mask.FieldMask
+
+func BenchmarkFieldMaskFromRequestBody(b *testing.B) {
+	input := `{` +
+		`"single_nested":				{"name": "bar",` +
+		`                           	 "amount": 10,` +
+		`                           	 "ok": "TRUE"},` +
+		`"uuid":						"6EC2446F-7E89-4127-B3E6-5C05E6BECBA7",` +
+		`"nested": 						[{"name": "bar",` +
+		`								  "amount": 10},` +
+		`								 {"name": "baz",` +
+		`								  "amount": 20}],` +
+		`"float_value":             	1.5,` +
+		`"double_value":            	2.5,` +
+		`"int64_value":             	4294967296,` +
+		`"uint64_value":            	9223372036854775807,` +
+		`"int32_value":             	-2147483648,` +
+		`"fixed64_value":           	9223372036854775807,` +
+		`"fixed32_value":           	4294967295,` +
+		`"bool_value":              	true,` +
+		`"string_value":            	"strprefix/foo",` +
+		`"bytes_value":					"132456",` +
+		`"uint32_value":            	4294967295,` +
+		`"enum_value":     		        "ONE",` +
+		`"path_enum_value":	    	    "DEF",` +
+		`"nested_path_enum_value":  	"JKL",` +
+		`"sfixed32_value":          	2147483647,` +
+		`"sfixed64_value":          	-4611686018427387904,` +
+		`"sint32_value":            	2147483647,` +
+		`"sint64_value":            	4611686018427387903,` +
+		`"repeated_string_value": 		["a", "b", "c"],` +
+		`"oneof_value":					{"oneof_string":"x"},` +
+		`"map_value": 					{"a": "ONE",` +
+		`								 "b": "ZERO"},` +
+		`"mapped_string_value": 		{"a": "x",` +
+		`								 "b": "y"},` +
+		`"mapped_nested_value": 		{"a": {"name": "x", "amount": 1},` +
+		`								 "b": {"name": "y", "amount": 2}},` +
+		`"nonConventionalNameValue":	"camelCase",` +
+		`"timestamp_value":				"2016-05-10T10:19:13.123Z",` +
+		`"repeated_enum_value":			["ONE", "ZERO"],` +
+		`"repeated_enum_annotation":	 ["ONE", "ZERO"],` +
+		`"enum_value_annotation": 		"ONE",` +
+		`"repeated_string_annotation":	["a", "b"],` +
+		`"repeated_nested_annotation": 	[{"name": "hoge",` +
+		`								  "amount": 10},` +
+		`								 {"name": "fuga",` +
+		`								  "amount": 20}],` +
+		`"nested_annotation": 			{"name": "hoge",` +
+		`								 "amount": 10},` +
+		`"int64_override_type":			12345` +
+		`}`
+	var r *field_mask.FieldMask
+	var err error
+	for i := 0; i < b.N; i++ {
+		r, err = FieldMaskFromRequestBody(bytes.NewReader([]byte(input)))
+	}
+	if err != nil {
+		b.Error(err)
+	}
+	result = r
+}
