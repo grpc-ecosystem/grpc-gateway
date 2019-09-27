7a37a556504c866c1d245d5482db47da02e9498c
Will Plano <51954103+william-plano-oxb@users.noreply.github.com>
2019-09-27 14:36:41 +0100

    Apply suggestions from code review
    Fix typos (again)
    
    Co-Authored-By: Johan Brandhorst <johan.brandhorst@gmail.com>

--- examples/integration/integration_test.go
+++ examples/integration/integration_test.go
@@ -735,7 +735,7 @@ func TestABEPatchBody(t *testing.T) {
 			},
 		},
 		{
-			// N.B. This case passes the empty field mask to the UpdateV2 method so falls back to PUT semantics as per the implementeation
+			// N.B. This case passes the empty field mask to the UpdateV2 method so falls back to PUT semantics as per the implementation.
 			name: "with empty fieldmask",
 			originalValue: gw.ABitOfEverything{
 				Int32Value:   42,
@@ -752,7 +752,7 @@ func TestABEPatchBody(t *testing.T) {
 			},
 		},
 		{
-			// N.B. This case passes the nil field mask to the UpdateV2 method so falls back to PUT semantics as per the implementeation
+			// N.B. This case passes the nil field mask to the UpdateV2 method so falls back to PUT semantics as per the implementation.
 			name: "with nil fieldmask",
 			originalValue: gw.ABitOfEverything{
 				Int32Value:   42,
