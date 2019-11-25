f35c3c8d9d5b1b5c8ab5247cceb89e2313807d0d
Yuchao Zhang <418121364@qq.com>
2019-11-25 17:01:37 +0800

    Add support for stream google.api.HttpBody

--- runtime/marshal_httpbodyproto.go
+++ runtime/marshal_httpbodyproto.go
@@ -1,6 +1,9 @@
 package runtime
 
 import (
+	"reflect"
+
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/genproto/googleapis/api/httpbody"
 )
 
@@ -12,9 +15,10 @@ func SetHTTPBodyMarshaler(serveMux *ServeMux) {
 }
 
 // HTTPBodyMarshaler is a Marshaler which supports marshaling of a
-// google.api.HttpBody message as the full response body if it is
-// the actual message used as the response. If not, then this will
-// simply fallback to the Marshaler specified as its default Marshaler.
+// google.api.HttpBody message as the full response body if it is the actual
+// message used as the response. Stream google.api.HttpBody message is also
+// supported. If not, then this will simply fallback to the Marshaler specified
+// as its default Marshaler.
 type HTTPBodyMarshaler struct {
 	Marshaler
 }
@@ -27,7 +31,7 @@ func (h *HTTPBodyMarshaler) ContentType() string {
 // ContentTypeFromMessage in case v is a google.api.HttpBody message it returns
 // its specified content type otherwise fall back to the default Marshaler.
 func (h *HTTPBodyMarshaler) ContentTypeFromMessage(v interface{}) string {
-	if httpBody, ok := v.(*httpbody.HttpBody); ok {
+	if httpBody := tryHttpBody(v); httpBody != nil {
 		return httpBody.GetContentType()
 	}
 	return h.Marshaler.ContentType()
@@ -36,8 +40,43 @@ func (h *HTTPBodyMarshaler) ContentTypeFromMessage(v interface{}) string {
 // Marshal marshals "v" by returning the body bytes if v is a
 // google.api.HttpBody message, otherwise it falls back to the default Marshaler.
 func (h *HTTPBodyMarshaler) Marshal(v interface{}) ([]byte, error) {
-	if httpBody, ok := v.(*httpbody.HttpBody); ok {
-		return httpBody.Data, nil
+	if httpBody := tryHttpBody(v); httpBody != nil {
+		return httpBody.GetData(), nil
 	}
 	return h.Marshaler.Marshal(v)
+}
+
+// Delimiter for encoded multi-part streams.
+func (h *HTTPBodyMarshaler) Delimiter() []byte {
+	return []byte("")
+}
+
+func tryHttpBody(v interface{}) *httpbody.HttpBody {
+	rv := reflect.ValueOf(v)
+	// The handler wraps streamed chunks in a map.
+	// If we're sending an HTTP body as a chunk, we need to unpack it.
+	if rv.Kind() == reflect.Map && rv.Type().ConvertibleTo(reflect.TypeOf(map[string]proto.Message{})) {
+		m := v.(map[string]proto.Message)
+		if r, ok := m["result"]; ok && proto.MessageName(r) == "google.api.HttpBody" {
+			return r.(*httpbody.HttpBody)
+		}
+		return nil
+	}
+	if rv.Kind() != reflect.Ptr {
+		return nil
+	}
+	for rv.Kind() == reflect.Ptr {
+		if rv.IsNil() {
+			rv.Set(reflect.New(rv.Type().Elem()))
+		}
+		if rv.Type().ConvertibleTo(typeProtoMessage) {
+			pb := rv.Interface().(proto.Message)
+			if proto.MessageName(pb) == "google.api.HttpBody" {
+				return v.(*httpbody.HttpBody)
+			}
+		}
+		rv = rv.Elem()
+	}
+	return nil
+
 }
