b3237cc42f6fef4be5f0bceb2f0de04bb7fb5658
Johan Brandhorst <johan.brandhorst@gmail.com>
2020-01-20 10:50:18 +0000

    docs: require Jekyll Github Token to be set
    This didn't run successfully without this for me

--- docs/run.sh
+++ docs/run.sh
@@ -18,11 +18,17 @@ if [ ! -d "${BUNDLE_DIR}" ]; then
     bundle update
 fi
 
+if [[ ${JEKYLL_GITHUB_TOKEN} == "" ]]; then
+  echo "Please set \$JEKYLL_GITHUB_TOKEN before running"
+  exit 1
+fi
+
 docker run --rm \
   --volume="${PWD}:/srv/jekyll" \
   -p 35729:35729 -p 4000:4000 \
   -e "JEKYLL_UID=$(id -u)" \
   -e "JEKYLL_GID=$(id -g)" \
+  -e "JEKYLL_GITHUB_TOKEN=${JEKYLL_GITHUB_TOKEN}" \
   --volume="/tmp/grpc-gateway-bundle:/usr/local/bundle" \
   -it "jekyll/builder:${JEKYLL_VERSION}" \
   jekyll serve
