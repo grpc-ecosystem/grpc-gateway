094b9133e1e2163820b453ea608921cd60d362d3
wp0pw <waldemar.parzonka@gmail.com>
2020-02-25 14:32:03 +0000

    update docs for configuring query parser
    Co-Authored-By: Johan Brandhorst <johan.brandhorst@gmail.com>

--- runtime/mux.go
+++ runtime/mux.go
@@ -56,6 +56,8 @@ func WithForwardResponseOption(forwardResponseOption func(context.Context, http.
 }
 
 // WithQueryParameterParser sets the query parameter parser, used to populate message from query parameters.
+// Configuring this will mean the generated swagger output is no longer correct, and it should be
+// done with careful consideration.
 func WithQueryParameterParser(queryParameterParser QueryParameterParser) ServeMuxOption {
 	return func(serveMux *ServeMux) {
 		currentQueryParser = queryParameterParser
