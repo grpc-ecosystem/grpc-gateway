fadb965e79db37f1cbd85190f7ec5c72214f12f9
Johan Brandhorst <johan.brandhorst@gmail.com>
2020-05-05 23:24:35 +0100

    runtime: rewrite query.go in new protoreflect

--- runtime/marshal_jsonpb.go
+++ runtime/marshal_jsonpb.go
@@ -260,3 +260,17 @@ var allowUnknownFields = true
 func DisallowUnknownFields() {
 	allowUnknownFields = false
 }
+
+var (
+	convFromType = map[reflect.Kind]reflect.Value{
+		reflect.String:  reflect.ValueOf(String),
+		reflect.Bool:    reflect.ValueOf(Bool),
+		reflect.Float64: reflect.ValueOf(Float64),
+		reflect.Float32: reflect.ValueOf(Float32),
+		reflect.Int64:   reflect.ValueOf(Int64),
+		reflect.Int32:   reflect.ValueOf(Int32),
+		reflect.Uint64:  reflect.ValueOf(Uint64),
+		reflect.Uint32:  reflect.ValueOf(Uint32),
+		reflect.Slice:   reflect.ValueOf(Bytes),
+	}
+)
