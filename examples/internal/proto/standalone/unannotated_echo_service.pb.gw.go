11764d51d8797db9e61deb178f75463bd6e7362c
Qiaosen Huang <joesonw@gmail.com>
2020-05-26 22:14:20 +0800

    Add warning note to RegisterXXXHandlerServer (#1401)
    * cherry-pick #1399
    
    * remove extra
    
    * add missing test result change

--- examples/internal/proto/standalone/unannotated_echo_service.pb.gw.go
+++ examples/internal/proto/standalone/unannotated_echo_service.pb.gw.go
@@ -593,6 +593,7 @@ func local_request_UnannotatedEchoService_EchoDelete_0(ctx context.Context, mars
 // RegisterUnannotatedEchoServiceHandlerServer registers the http handlers for service UnannotatedEchoService to "mux".
 // UnaryRPC     :call UnannotatedEchoServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
+// Note that using this registration option will cause many gRPC library features (such as grpc.SendHeader, etc) to stop working. Consider using RegisterUnannotatedEchoServiceHandlerFromEndpoint instead.
 func RegisterUnannotatedEchoServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server extExamplepb.UnannotatedEchoServiceServer) error {
 
 	mux.Handle("POST", pattern_UnannotatedEchoService_Echo_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
