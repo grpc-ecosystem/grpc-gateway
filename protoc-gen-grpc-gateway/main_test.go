0693088c48ee61a01131d805f3acf8f2c8ef7f9c
Daniel Hochman <dhochman@lyft.com>
2020-10-15 11:09:10 -0500

    update test after merge

--- protoc-gen-grpc-gateway/main_test.go
+++ protoc-gen-grpc-gateway/main_test.go
@@ -2,26 +2,21 @@ package main
 
 import (
 	"testing"
-
-	"github.com/grpc-ecosystem/grpc-gateway/v2/internal/descriptor"
 )
 
 func TestParseFlagsEmptyNoPanic(t *testing.T) {
-	reg := descriptor.NewRegistry()
-	parseFlags(reg, "")
+	parseFlags("")
 }
 
 func TestParseFlags(t *testing.T) {
-	reg := descriptor.NewRegistry()
-	parseFlags(reg, "allow_repeated_fields_in_body=true")
+	parseFlags("allow_repeated_fields_in_body=true")
 	if *allowRepeatedFieldsInBody != true {
 		t.Errorf("flag allow_repeated_fields_in_body was not set correctly, wanted true got %v", *allowRepeatedFieldsInBody)
 	}
 }
 
 func TestParseFlagsMultiple(t *testing.T) {
-	reg := descriptor.NewRegistry()
-	parseFlags(reg, "allow_repeated_fields_in_body=true,import_prefix=foo")
+	parseFlags("allow_repeated_fields_in_body=true,import_prefix=foo")
 	if *allowRepeatedFieldsInBody != true {
 		t.Errorf("flag allow_repeated_fields_in_body was not set correctly, wanted 'true' got '%v'", *allowRepeatedFieldsInBody)
 	}
