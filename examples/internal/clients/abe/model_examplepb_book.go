509841586e7b55b26ffe82a75d4aad057e144ad9
HP <alperengozeten@gmail.com>
2021-08-31 11:18:56 +0300

    bazel build

--- examples/internal/clients/abe/model_examplepb_book.go
+++ examples/internal/clients/abe/model_examplepb_book.go
@@ -14,9 +14,7 @@ import (
 	"time"
 )
 
-// An example resource type from AIP-123 used to test the behavior described in the CreateBookRequest message.  See: https://google.aip.dev/123
 type ExamplepbBook struct {
-	// The resource name of the book.  Format: `publishers/{publisher}/books/{book}`  Example: `publishers/1257894000000000000/books/my-book`
 	Name string `json:"name,omitempty"`
 	// Output only. The book's ID.
 	Id string `json:"id,omitempty"`
