6ef34aa4630f80f7696433b9ff74e030a2090050
Makdon <makdon@makdon.me>
2021-12-07 09:02:54 +0800

    Log warning if HeaderMatcherFunc passes malformed HTTP headers to grpc server (#2455)
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

--- runtime/context.go
+++ runtime/context.go
@@ -41,6 +41,12 @@ var (
 	DefaultContextTimeout = 0 * time.Second
 )
 
+// malformedHTTPHeaders lists the headers that the gRPC server may reject outright as malformed.
+// See https://github.com/grpc/grpc-go/pull/4803#issuecomment-986093310 for more context.
+var malformedHTTPHeaders = map[string]struct{}{
+	"connection": {},
+}
+
 type (
 	rpcMethodKey       struct{}
 	httpPathPatternKey struct{}
@@ -306,6 +312,13 @@ func isPermanentHTTPHeader(hdr string) bool {
 		return true
 	}
 	return false
+}
+
+// isMalformedHTTPHeader checks whether header belongs to the list of
+// "malformed headers" and would be rejected by the gRPC server.
+func isMalformedHTTPHeader(header string) bool {
+	_, isMalformed := malformedHTTPHeaders[strings.ToLower(header)]
+	return isMalformed
 }
 
 // RPCMethod returns the method string for the server context. The returned
