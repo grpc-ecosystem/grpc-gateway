61facaa2d2109142d421709af9fb6e6a2755f225
GreyXor <79602273+GreyXor@users.noreply.github.com>
2022-05-04 17:23:02 +0200

    fix: Add Content-Type header to the health endpoint (#2634)
    * fix: Content-Type and Grpc-Metadata-Content-Type headers with the health endpoint #2611
    
    * fix: no need to set grpc content type header
    
    * fix: remove unnecessary grpc header comment
    
    * test: lighten the Content-Type header test

--- runtime/mux.go
+++ runtime/mux.go
@@ -229,6 +229,8 @@ func WithHealthEndpointAt(healthCheckClient grpc_health_v1.HealthClient, endpoin
 					return
 				}
 
+				w.Header().Set("Content-Type", "application/json")
+
 				if resp.GetStatus() != grpc_health_v1.HealthCheckResponse_SERVING {
 					var err error
 					switch resp.GetStatus() {
