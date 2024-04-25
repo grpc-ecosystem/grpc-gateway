c5d4e7507863ed6cb6ca7584c09b813be1f44e16
Joshua Garnett <josh.garnett@gmail.com>
2024-04-25 18:56:49 -0400

    Removing the option, always writing Content-Length if doForwardTrailers is not true

--- runtime/handler.go
+++ runtime/handler.go
@@ -177,7 +177,7 @@ func ForwardResponseMessage(ctx context.Context, mux *ServeMux, marshaler Marsha
 		return
 	}
 
-	if !doForwardTrailers && mux.writeContentLength {
+	if !doForwardTrailers {
 		w.Header().Set("Content-Length", strconv.Itoa(len(buf)))
 	}
 
