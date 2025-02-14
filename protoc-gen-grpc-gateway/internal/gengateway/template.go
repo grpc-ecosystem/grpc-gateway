7aeecfb224a16975fee70f4161185c81eba9ee18
Paško Zdilar <53006174+paskozdilar@users.noreply.github.com>
2025-02-14 18:43:35 +0100

    Explicitly discard body instead of reading it
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

--- protoc-gen-grpc-gateway/internal/gengateway/template.go
+++ protoc-gen-grpc-gateway/internal/gengateway/template.go
@@ -382,7 +382,7 @@ var filter_{{ .Method.Service.GetName }}_{{ .Method.GetName }}_{{ .Index }} = {{
 	}
 	{{- end }}
 {{- else }}
-    io.ReadAll(req.Body)
+    io.Copy(io.Discard, req.Body)
 {{- end }}
 {{- if .PathParams }}
 	{{- $binding := . }}
