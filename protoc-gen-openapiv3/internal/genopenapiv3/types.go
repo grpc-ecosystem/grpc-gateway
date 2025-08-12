88ed4243d1539e3b97b21263c5be0a9d38baf6fc
Amirsalar Safaei <amirsalar.safaei@divar.ir>
2025-08-12 21:57:47 +0330

    fix: generation path and encoder indentation

--- protoc-gen-openapiv3/internal/genopenapiv3/types.go
+++ protoc-gen-openapiv3/internal/genopenapiv3/types.go
@@ -1,7 +1,10 @@
 package genopenapiv3
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"google.golang.org/grpc/grpclog"
@@ -17,21 +20,38 @@ const (
 
 var ErrUnSupportedFormat = errors.New("unsupported format provided")
 
+type Encoder interface {
+	Encode(v any) error
+}
 
 func (f Format) MarshalOpenAPIDoc(doc *openapi3.T) ([]byte, error) {
+
+	openapiDoc, err := doc.MarshalYAML()
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	var encoder Encoder
+
 	switch f {
 	case FormatJSON:
-		return doc.MarshalJSON()
+		encoder = json.NewEncoder(&buf)
+		encoder.(*json.Encoder).SetIndent("", "  ")
 	case FormatYAML:
-		openapiDoc, err := doc.MarshalYAML()
-		if err != nil {
-			return nil, err
-		}
-		return yaml.Marshal(openapiDoc)
+		encoder = yaml.NewEncoder(&buf)
+		encoder.(*yaml.Encoder).SetIndent(2)
 	default:
 		grpclog.Errorf("unsupported format: %s\n", f)
 		return nil, ErrUnSupportedFormat
 	}
+
+	err = encoder.Encode(openapiDoc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode OpenAPI document: %w", err)
+	}
+
+	return buf.Bytes(), nil
 }
 
 type OneOfStrategy string
