dd46c782ee3ec3b40ec9115e7756ae3753bcf0b1
abhipranay <abhipranay.chauhan@gmail.com>
2026-03-09 02:04:41 +0100

    refactor(openapiv3): add generic getAnnotation helper function

--- protoc-gen-openapiv3/internal/genopenapiv3/annotations.go
+++ protoc-gen-openapiv3/internal/genopenapiv3/annotations.go
@@ -4,19 +4,32 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/internal/descriptor"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv3/options"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
+
+// getAnnotation is a generic function that retrieves OpenAPI v3 annotations from proto options.
+// It reduces code duplication by handling the common pattern of:
+// 1. Checking if options is nil
+// 2. Getting the extension
+// 3. Type asserting to the expected type
+func getAnnotation[T any](opts proto.Message, ext protoreflect.ExtensionType) *T {
+	if opts == nil {
+		return nil
+	}
+	extVal := proto.GetExtension(opts, ext)
+	result, ok := extVal.(*T)
+	if !ok || result == nil {
+		return nil
+	}
+	return result
+}
 
 // getFileAnnotation retrieves the OpenAPI v3 file-level annotation if present.
 func getFileAnnotation(file *descriptor.File) *options.OpenAPI {
 	if file.Options == nil {
 		return nil
 	}
-	ext := proto.GetExtension(file.Options, options.E_Openapiv3Document)
-	opts, ok := ext.(*options.OpenAPI)
-	if !ok || opts == nil {
-		return nil
-	}
-	return opts
+	return getAnnotation[options.OpenAPI](file.Options, options.E_Openapiv3Document)
 }
 
 // getMethodAnnotation retrieves the OpenAPI v3 method-level annotation if present.
@@ -24,12 +37,7 @@ func getMethodAnnotation(method *descriptor.Method) *options.Operation {
 	if method.Options == nil {
 		return nil
 	}
-	ext := proto.GetExtension(method.Options, options.E_Openapiv3Operation)
-	opts, ok := ext.(*options.Operation)
-	if !ok || opts == nil {
-		return nil
-	}
-	return opts
+	return getAnnotation[options.Operation](method.Options, options.E_Openapiv3Operation)
 }
 
 // getMessageAnnotation retrieves the OpenAPI v3 message-level annotation if present.
@@ -37,12 +45,7 @@ func getMessageAnnotation(msg *descriptor.Message) *options.Schema {
 	if msg.Options == nil {
 		return nil
 	}
-	ext := proto.GetExtension(msg.Options, options.E_Openapiv3Schema)
-	opts, ok := ext.(*options.Schema)
-	if !ok || opts == nil {
-		return nil
-	}
-	return opts
+	return getAnnotation[options.Schema](msg.Options, options.E_Openapiv3Schema)
 }
 
 // getFieldAnnotation retrieves the OpenAPI v3 field-level annotation if present.
@@ -50,12 +53,7 @@ func getFieldAnnotation(field *descriptor.Field) *options.Schema {
 	if field.Options == nil {
 		return nil
 	}
-	ext := proto.GetExtension(field.Options, options.E_Openapiv3Field)
-	opts, ok := ext.(*options.Schema)
-	if !ok || opts == nil {
-		return nil
-	}
-	return opts
+	return getAnnotation[options.Schema](field.Options, options.E_Openapiv3Field)
 }
 
 // getServiceAnnotation retrieves the OpenAPI v3 service-level annotation if present.
@@ -63,12 +61,7 @@ func getServiceAnnotation(svc *descriptor.Service) *options.Tag {
 	if svc.Options == nil {
 		return nil
 	}
-	ext := proto.GetExtension(svc.Options, options.E_Openapiv3Tag)
-	opts, ok := ext.(*options.Tag)
-	if !ok || opts == nil {
-		return nil
-	}
-	return opts
+	return getAnnotation[options.Tag](svc.Options, options.E_Openapiv3Tag)
 }
 
 // getEnumAnnotation retrieves the OpenAPI v3 enum-level annotation if present.
@@ -76,10 +69,5 @@ func getEnumAnnotation(enum *descriptor.Enum) *options.EnumSchema {
 	if enum.Options == nil {
 		return nil
 	}
-	ext := proto.GetExtension(enum.Options, options.E_Openapiv3Enum)
-	opts, ok := ext.(*options.EnumSchema)
-	if !ok || opts == nil {
-		return nil
-	}
-	return opts
+	return getAnnotation[options.EnumSchema](enum.Options, options.E_Openapiv3Enum)
 }
